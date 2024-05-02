4df5c4d734ed2cd1c86df8d136f2ade8f333a2c0
flavio088 <flavio088gomes@gmail>
2024-05-02 19:27:15 -0300

    efeito blur nos links

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -8,7 +8,7 @@ export default function Profile(props){
         <div>{props.bio}</div>
         <div>{props.phone}</div>
         <div>{props.email}</div>
-        <div>
+        <div className={styles.links}>
             <a href={props.githubUrl} target="_blank">Github</a>
             <a href={props.linkedinUrl} target="_blank">LindekIn</a>
             <a href={props.twitterUrl} target="_blank">Twitter</a>
