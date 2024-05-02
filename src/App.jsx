cd740c2516aea753b828965ee895c5b013411446
flavio088 <flavio088gomes@gmail>
2024-05-02 18:25:37 -0300

    ProfileReact

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Profile from "./components/Profile";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Profile 
+        avatar="https://media.licdn.com/dms/image/D4D03AQEPS_Pr9juhbg/profile-displayphoto-shrink_200_200/0/1688042961355?e=1720051200&v=beta&t=LTk5wvSpR9nN-Pt0X9J8YHlEv0SgU8NCPYfJEHfTPSg"
+        name="Flávio Gomes"
+        bio="Dev. Frontend"
+        email="flavio088gomes@gmail.com"
+        phone="+55 (81)9 9708-6895"
+        githubUrl="https://github.com/flavio088"
+        linkedinUrl="https://www.linkedin.com/in/flaviogomes0808"
+        twitterUrl="https://twitter.com"
+      />
+    </div>
+  )
+}
+
